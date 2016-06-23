d7baa5a1290a5ac4ca24be31687602928bc6ce7b
Kazuhro Ota <kazufsaf@gmail.com>
2016-06-23 14:34:50 +0900

    Fix emacs Update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -24,7 +24,7 @@
 ;;; package
 (require 'package)
 ;(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 (package-initialize)
 
 ;;; quelpa
